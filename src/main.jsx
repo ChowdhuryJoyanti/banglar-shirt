e4e59515a7f1d91592d18340b93fc212e3a49ee5
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-04-10 20:50:52 +0600

    grandpa added

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import {
 import Home from './Components/Home/Home';
 import Main from './Components/Layout/Main';
 import OrderReview from './Components/OrderReview/OrderReview';
+import Grandpa from './Components/Grandpa/Grandpa';
 
 const router = createBrowserRouter([
 
@@ -25,7 +26,13 @@ const router = createBrowserRouter([
     {
       path:'/review',
       element:<OrderReview></OrderReview>,
-    }
+    },
+    
+    {
+      path:'/grandpa',
+      element:<Grandpa></Grandpa>
+    },
+
   ]
 
 }
